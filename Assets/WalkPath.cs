d541b0e42bb66728bb3360e9c6178d082206c89a
svc-student <svc-student@student.directory.jccc>
2019-05-14 19:20:21 -0500

    worked on stuff

--- Assets/WalkPath.cs
+++ Assets/WalkPath.cs
@@ -2,31 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(KinematicSeek))]
 public class WalkPath : MonoBehaviour
 {
+  public PathControl pathToWalk;
+  private int waypointIndex = 0;
+  private KinematicSeek kinematicSeek;
+  private RotatingQue<Vector3> _waypoints;
+  private void Awake()
+  {
+    kinematicSeek = GetComponent<KinematicSeek>();
+  }
 
-    public PathControl pathToWalk;
-    private int waypointIndex = 0;
-    private KinematicSeek kinematicSeek;
-
-    void Start()
+  void Start()
+  {
+    List<Vector3> waypointLocations = pathToWalk.Waypoints;
+    for (int i = 1; i < waypointLocations.Count; i++)
     {
-        kinematicSeek = GetComponent<KinematicSeek>();
-
-        List<Vector3> waypointLocations = pathToWalk.Waypoints;
-        for (int i = 1; i < waypointLocations.Count; i++)
-        {
-            if (Vector3.Distance(transform.position, waypointLocations[i]) < Vector3.Distance(transform.position, waypointLocations[waypointIndex]))
-            {
-                waypointIndex = i;
-            }
-        }
+      // Get the closest point
+      if (Vector3.Distance(transform.position, waypointLocations[i]) < Vector3.Distance(transform.position, waypointLocations[waypointIndex]))
+        waypointIndex = i;
     }
 
-    // Update is called once per frame
-    void Update()
+    _waypoints = new RotatingQue<Vector3>(pathToWalk.Waypoints);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (kinematicSeek.IsAtTarget)
+      kinematicSeek.destination = _waypoints.Next;
+  }
+}
+
+public class RotatingQue<T>
+{
+  private List<T> _que = new List<T>();
+  private int currentIndex = 0;
+
+  public RotatingQue(List<T> que, int startingIndex = 0)
+  {
+    _que = que;
+    currentIndex = startingIndex;
+  }
+
+  public T Next
+  {
+    get
     {
-        if (kinematicSeek.IsAtTarget) waypointIndex = (waypointIndex + 1) % pathToWalk.Waypoints.Count;
-        kinematicSeek.destination = pathToWalk.Waypoints[waypointIndex];
+      currentIndex = currentIndex + 1 < Count ? currentIndex + 1 : 0; 
+      return _que[currentIndex];
     }
+  }
+
+  public int Count
+  {
+    get { return _que.Count; }
+  }
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -5,31 +5,13 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  private void OnDrawGizmosSelected()
+  {
+    transform.parent.GetComponent<PathControl>().DrawPathGizmos();
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        // Draw gizmos to all waypoints in order;
-        // that means we need something that controls the order of the waypoints, yes yes?
-        // So we do need to create a path.
-        // 
-        // Draw from the parent
-
-        transform.parent.GetComponent<PathControl>().DrawPathGizmos();
-    }
-
-    private void OnDestroy()
-    {
-        transform.parent.GetComponent<PathControl>().ResetWaypointNames();
-    }
+  private void OnDestroy()
+  {
+    transform.parent.GetComponent<PathControl>().ResetWaypointNames();
+  }
 }
